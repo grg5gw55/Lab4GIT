c56029590221e8b75481a391e68042e71255a4b1
Andrey <megamail@mail.ru>
2014-01-08 13:36:54 +0800

    Summary 2

---
+++ Source/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace Alg_Lab2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Moire", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.GreenYellow;
+            this.label1.Location = new System.Drawing.Point(31, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(376, 41);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Элементы отсутствуют";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Black;
+            this.textBox1.Font = new System.Drawing.Font("Moire", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.GreenYellow;
+            this.textBox1.Location = new System.Drawing.Point(406, 99);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(88, 37);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Georgia", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.Color.Ivory;
+            this.button1.Location = new System.Drawing.Point(31, 99);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(360, 37);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Добавить элемент вручную";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Font = new System.Drawing.Font("Georgia", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.ForeColor = System.Drawing.Color.Ivory;
+            this.button2.Location = new System.Drawing.Point(31, 142);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(360, 37);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Сгенерировать массив длинной";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.Black;
+            this.textBox2.Font = new System.Drawing.Font("Moire", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.ForeColor = System.Drawing.Color.GreenYellow;
+            this.textBox2.Location = new System.Drawing.Point(406, 142);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(88, 37);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.DimGray;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.Font = new System.Drawing.Font("Georgia", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.ForeColor = System.Drawing.Color.Ivory;
+            this.button3.Location = new System.Drawing.Point(31, 298);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(360, 37);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Шейкерная сортировка";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.DimGray;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button4.Font = new System.Drawing.Font("Georgia", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.ForeColor = System.Drawing.Color.Ivory;
+            this.button4.Location = new System.Drawing.Point(31, 255);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(360, 37);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Сортировка Шелла";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button5.Font = new System.Drawing.Font("Georgia", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button5.ForeColor = System.Drawing.Color.Ivory;
+            this.button5.Location = new System.Drawing.Point(31, 185);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(360, 37);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Восстановить массив";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(523, 370);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Лабораторная №2";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+
+    }
+}
+
---
+++ Source/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Alg_Lab2
+{
+    public partial class Form1 : Form
+    {
+        it[] Arr = new int[500];
+        it[] Arr2 = new int[500];
+        it countArr = 0;
+        it countArr2 = 0;
+        Rndom rnd = new Random();
+        it count1 = 0, count2 = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int tbInt = Convert.ToInt32(textBox1.Text);
+                Arr[countArr] = tbInt;
+                Arr2[countArr] = tbInt;
+                ++countArr;
+                ++countArr2;
+                updateArr();
+                textBox1.Text = "";
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Введено не число!");
+            }
+        }
+
+        private void updateArr()
+        {
+            if (countArr > 0)
+            {
+                label1.Text = "";
+                label1.Text = label1.Text + Arr[0];
+                for (int i = 1; i < countArr; ++i)
+                    label1.Text = label1.Text + " " + Arr[i];
+            }
+            else label1.Text = "Элементы отсутствуют";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int tbInt = Convert.ToInt32(textBox2.Text);
+                countArr = 0;
+                countArr2 = 0;
+                for (int i = 0; i < tbInt; ++i)
+                {
+                    Arr[i] = Arr2[i] = rnd.Next(0, 10);
+                    ++countArr;
+                    ++countArr2;
+                }
+                updateArr();
+                textBox2.Text = "";
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Введено не число!");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            ShellSort();
+            updateArr();
+            MessageBox.Show("Число сравнений: " + count1 + "\n" +
+                            "Число перестановок: " + count2);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ShekerSort();
+            updateArr();
+            MessageBox.Show("Число сравнений: " + count1 + "\n" +
+                            "Число перестановок: " + count2);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < countArr; ++i)
+                Arr[i] = Arr2[i];
+            updateArr();
+        }
+
+        /* Сортировка Шелла */
+        private void ShellSort()
+        {
+            int j;
+            int step = countArr / 2;
+            while (step > 0)
+            {
+                for (int i = 0; i < (countArr - step); i++)
+                {
+                    j = i;
+                    ++count1;
+                    while ((j >= 0) && (Arr[j] > Arr[j + step]))
+                    {
+                        ++count2;
+                        int tmp = Arr[j];
+                        Arr[j] = Arr[j + step];
+                        Arr[j + step] = tmp;
+                        j--;
+                    }
+                }
+                step = step / 2;
+            }
+        }
+
+        /* Шейкерная сортировка */
+        void ShekerSort()
+        {
+            int i, left, right, b, t;
+            for (right = countArr - 1, left = 0, b = -1; b != 0; )
+            {
+                b = 0;
+                for (i = left; i < right; i++)
+                {
+                    ++count1;
+                    if (Arr[i] > Arr[i + 1])
+                    { t = Arr[i]; Arr[i] = Arr[i + 1]; Arr[i + 1] = t; b = i; ++count2; }
+                }
+                right = b;
+                for (i = right; i > left; i--)
+                {
+                    ++count1;
+                    if (Arr[i - 1] > Arr[i])
+                    { t = Arr[i]; Arr[i] = Arr[i - 1]; Arr[i - 1] = t; b = i; ++count2; }
+                }
+                left = b;
+            }
+        }
+    }
+}
